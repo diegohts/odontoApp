4e4d367a830ad1725948c284780a55adc0b0ce69
Diego Henrique <diegohrp20@gmail.com>
2023-02-14 03:13:00 -0300

    Autenticando o usuário e realizando ajuste, pois o spring executa primeiro o filtro dele referente ao UsernamePasswordAuthenticationFilter, com isso fizemos para executarmos o nosso primeiro. E realizamos os testes de controle de acesso.

--- api/src/main/java/com/odontoApp/api/infra/security/SecurityFilter.java
+++ api/src/main/java/com/odontoApp/api/infra/security/SecurityFilter.java
@@ -5,12 +5,20 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.odontoApp.api.domain.usuario.UsuarioRepository;
+
 import java.io.IOException;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Autowired
 	private TokenService tokenService;
@@ -18,7 +26,14 @@ public class SecurityFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
+
+		if(tokenJWT != null) {
+			var subject = tokenService.getSubject(tokenJWT);
+			var usuario = usuarioRepository.findBylogin(subject);
+
+			var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
 
 		filterChain.doFilter(request, response);
 	}
@@ -26,10 +41,10 @@ public class SecurityFilter extends OncePerRequestFilter {
 	private String recuperarToken(HttpServletRequest request) {
 		var authorizationHeader = request.getHeader("Authorization");
 
-		if(authorizationHeader == null) {
-			throw new RuntimeException("Token não enviado no cabeçalho Authorization!");
+		if(authorizationHeader != null) {
+			return authorizationHeader.replace("Bearer ", "");
 		}
 
-		return authorizationHeader.replace("Bearer ", "");
+		return null;
 	}
 }
