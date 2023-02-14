707340255085c406a9a2f0ace25ecd6cbf688f4a
Diego Henrique <diegohrp20@gmail.com>
2023-02-14 02:43:52 -0300

    Implementando o processo de autenticação na API, de maneira stateless, utilizando as classes e configurações do Spring Security.

---
+++ api/src/main/java/com/odontoApp/api/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package com.odontoApp.api.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
--- api/src/main/java/com/odontoApp/api/domain/usuario/Usuario.java
+++ api/src/main/java/com/odontoApp/api/domain/usuario/Usuario.java
@@ -1,5 +1,10 @@
 package com.odontoApp.api.domain.usuario;
 
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -12,11 +17,46 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
 
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String login;
 	private String senha;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
