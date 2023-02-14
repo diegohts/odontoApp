8bea63c90761f9f79c2bb35438f1c2b7c48cd97f
Diego Henrique <diegohrp20@gmail.com>
2023-02-14 02:21:15 -0300

    Criação das classes UsuarioRepository e AutenticacaoService.

---
+++ api/src/main/java/com/odontoApp/api/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.odontoApp.api.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+	UserDetails findBylogin(String login);
+}
