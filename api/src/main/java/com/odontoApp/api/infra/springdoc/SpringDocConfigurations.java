7b7d15dd455ed0f6dcdc4edf364b3348d467e779
Diego Henrique <diegohrp20@gmail.com>
2023-04-11 23:45:43 -0300

    Personalizando a documentação, colocando informações mais completas a respeito da API, como sua descrição, informações de contato e de sua licença de uso.

--- api/src/main/java/com/odontoApp/api/infra/springdoc/SpringDocConfigurations.java
+++ api/src/main/java/com/odontoApp/api/infra/springdoc/SpringDocConfigurations.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SpringDocConfigurations {
@@ -14,6 +17,15 @@ public class SpringDocConfigurations {
 		return new OpenAPI()
 			.components(new Components()
 				.addSecuritySchemes("bearer-key",
-					new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+				new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+				.info(new Info()
+					.title("OdontoApp API")
+					.description("API Rest da aplicação odontoApp, contendo as funcionalidades de CRUD de dentistas e pacientes, de informações sobre convênios, procedimentos e histórico clínico, além do agendamento e cancelamento de consultas")
+					.contact(new Contact()
+							.name("Diego Henrique")
+							.email("diegohenriquedev@gmail.com"))
+ 				.license(new License()
+					.name("Apache 2.0")
+					.url("http://odontoApp/api/licenca")));
 	}
 }
