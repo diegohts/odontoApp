34732465e5c613570fc7a3cd8041d7daa339784f
Diego Henrique <diegohrp20@gmail.com>
2023-04-12 00:01:02 -0300

    Excluindo comentários desnecessários.

--- api/src/test/java/com/odontoApp/api/domain/dentista/DentistaRepositoryTest.java
+++ api/src/test/java/com/odontoApp/api/domain/dentista/DentistaRepositoryTest.java
@@ -7,10 +7,7 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE) // Por padrão, o Spring não usará o mesmo banco de dados
-// da aplicação, mas um banco de dados in-memory, ou seja, um banco embutido (embedded), o H2.
-//estamos indicando que queremos fazer uma configuração do banco de dados de teste e pedindo para não substituir as configurações do
-//banco de dados pelas do banco em memória
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class DentistaRepositoryTest {
 
