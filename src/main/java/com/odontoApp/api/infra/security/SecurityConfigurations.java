7bb5a3bcbdd2ad6028d2627a696940560a6ac25d
Diego Henrique <diegohrp20@gmail.com>
2023-05-26 00:51:02 -0300

    Ajustando a classe de configurações de segurança para o tratamento do cors.

--- src/main/java/com/odontoApp/api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/odontoApp/api/infra/security/SecurityConfigurations.java
@@ -1,8 +1,10 @@
 package com.odontoApp.api.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -34,19 +35,33 @@ public class SecurityConfigurations {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf().disable()
-				// .cors(Customizer.withDefaults())
+		return http
+				.cors()
+					.configurationSource(corsConfigurationSource())
+					.and()
+				.csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().authorizeHttpRequests()
-				.requestMatchers(HttpMethod.POST, "/user/login", "/user/signup").permitAll()
-				.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-				.anyRequest().authenticated()
-				.and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+				.and()
+				.authorizeRequests()
+					.requestMatchers(request ->
+							request.getMethod().equals(HttpMethod.POST.name()) &&
+							(request.getServletPath().equals("/user/login") ||
+							request.getServletPath().equals("/user/signup"))
+					).permitAll()
+					.requestMatchers(request ->
+							request.getServletPath().startsWith("/v3/api-docs") ||
+							request.getServletPath().equals("/swagger-ui.html") ||
+							request.getServletPath().startsWith("/swagger-ui/")
+					).permitAll()
+					.anyRequest().authenticated()
+				.and()
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
 
@@ -59,9 +74,11 @@ public class SecurityConfigurations {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// configuration.setAllowedOrigins(Arrays.asList(appWebUrl));
+		configuration.setAllowedOrigins(Arrays.asList("*")); // permitir todos os domínios
 		configuration.setAllowedMethods(
 				Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
