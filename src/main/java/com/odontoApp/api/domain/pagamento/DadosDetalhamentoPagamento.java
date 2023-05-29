3961d70d0c8319777e348e01d26c982d85929654
Diego Henrique <diegohrp20@gmail.com>
2023-05-29 16:12:25 -0300

    Fornecer informações mais detalhadas na listagem de pagamentos realizados.

--- src/main/java/com/odontoApp/api/domain/pagamento/DadosDetalhamentoPagamento.java
+++ src/main/java/com/odontoApp/api/domain/pagamento/DadosDetalhamentoPagamento.java
@@ -3,9 +3,14 @@ package com.odontoApp.api.domain.pagamento;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
 
-public record DadosDetalhamentoPagamento(Long id, Long idConsulta, LocalDateTime dataPagamento, FormaPagamento formaPagamento, BigDecimal valorPago, String quemPagou, String observacao) {
+public record DadosDetalhamentoPagamento(Long id, Long idConsulta, String nomeDentista, String nomePaciente, String nomeProcedimento,
+	LocalDateTime dataConsulta, LocalDateTime dataPagamento, FormaPagamento formaPagamento,
+	BigDecimal valorPago, String quemPagou, String observacao) {
 
-	public DadosDetalhamentoPagamento(Pagamento pagamento) {
-		this(pagamento.getId(), pagamento.getConsulta().getId(), pagamento.getDataPagamento(), pagamento.getFormaPagamento(), pagamento.getValorPago(), pagamento.getQuemPagou(), pagamento.getObservacao());
-	}
+		public DadosDetalhamentoPagamento(Pagamento pagamento) {
+			this(pagamento.getId(), pagamento.getConsulta().getId(), pagamento.getConsulta().getDentista().getNome(),
+			pagamento.getConsulta().getPaciente().getNome(), pagamento.getConsulta().getProcedimento().getNome(),
+			pagamento.getConsulta().getData(), pagamento.getDataPagamento(), pagamento.getFormaPagamento(),
+			pagamento.getValorPago(), pagamento.getQuemPagou(), pagamento.getObservacao());
+		}
 }
